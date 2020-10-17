53c2771e68aeaa4aa30673d54f22474ffe3e6392
dev-sandrosantos <dev.sandrosantos@gmail.com>
2020-10-17 12:46:38 -0300

    ajuste tela my_Scan

--- lib/pages/vendedor_logado/my_scan.dart
+++ lib/pages/vendedor_logado/my_scan.dart
@@ -1,24 +1,60 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:migrou_app/http/webClients/MovimentacaoWebClient.dart';
+import 'package:migrou_app/pages/LoginPageAPI.dart';
+import 'package:migrou_app/utils/definicoes.dart';
+import 'package:http/http.dart' as http;
 
 class MyScanCode extends StatefulWidget {
   @override
   _MyScanCodeState createState() => _MyScanCodeState();
 }
 
-class _MyScanCodeState extends State<MyScanCode> {
-  String codeValue, value = "";
+String codeValue, value = "";
+Future creatUser(String idCliente, String idVendedor) async {
+  var idCliente = value;
+  var idVendedor = userId;
+  final headers = {
+    "Accept": "application/json",
+    'Content-Type': 'application/json',
+    'userSession': Constantes.TOKEN_ID
+  };
+  final body = jsonEncode({
+    "cliente": {"idCliente": idCliente},
+    "vendedor": {"idVendedor": idVendedor}
+  });
+  final String urlAPI = "${Constantes.HOST_DOMAIN}/vendedor/vinculaCliente";
+  final response = await http.patch(urlAPI, headers: headers, body: body);
 
+  if (response.statusCode == 200) {
+    final String responseString = response.body;
+    return responseString;
+  } else {
+    print("${response.statusCode} e ${response.headers}");
+    return null;
+  }
+}
+
+class _MyScanCodeState extends State<MyScanCode> {
   Future scan() async {
-    print(value);
     codeValue = await FlutterBarcodeScanner.scanBarcode(
         "#004297", "Cancelar", true, ScanMode.QR);
     setState(() {
-      value = codeValue;
+      idCliente = codeValue;
+      idVendedor = userId;
     });
   }
 
+  // ignore: unused_field
+  var _user;
+
   @override
+  void dispose() {
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
@@ -29,8 +65,21 @@ class _MyScanCodeState extends State<MyScanCode> {
             ),
             Center(
               child: Text(
-                "$value",
+                "$idCliente \n $idVendedor",
                 style: TextStyle(color: Colors.black),
+              ),
+            ),
+            Center(
+              child: FlatButton(
+                child: Text("Adicionar Usuario"),
+                onPressed: () async {
+                  idCliente = codeValue;
+                  idVendedor = userId;
+                  final user = await creatUser(idCliente, idVendedor);
+                  setState(() {
+                    _user = user;
+                  });
+                },
               ),
             )
           ],
